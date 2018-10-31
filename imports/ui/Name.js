b6bc58bdf5169f42c2ddadd52c8a8678910b127e
echau4 <31198647+echau4@users.noreply.github.com>
2018-10-31 10:56:55 -0500

    Added update and delete for the form

--- imports/ui/Name.js
+++ imports/ui/Name.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+import { Names } from '../api/names.js';
+
 // Name component - represents a single todo item 
 export default class Name extends Component {
+    toggleChecked() {
+        // Set the checked property to the opposite of its current value
+        Names.update(this.props.name._id, {
+            $set: { checked: !this.props.name.checked }, 
+        });
+    }
+    
+    deleteThisName() {
+        Names.remove(this.props.name._id);
+    }
+    
     render() {
+        // Give tasks a different className when they are checked off,
+        // so that we can style them nicely in css
+        const nameClassName = this.props.name.checked ? 'checked' : '';
+
         return (
-            <li>{this.props.name.firstName}</li>
+            <li className={nameClassName}>
+                <button className="delete" onClick={this.deleteThisName.bind(this)}>
+                    &times;
+                </button> 
+                
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.name.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className="text">{this.props.name.firstName} {this.props.name.lastName}</span>
+            </li>
         );
     }
 }
