f8a54c48ee6616f7fa81228aa39663923a127126
echau4 <31198647+echau4@users.noreply.github.com>
2018-12-05 09:23:53 -0600

    removed insecure from meteor app and added methods to insert, delete, and update in the app to replace it

--- imports/api/names.js
+++ imports/api/names.js
@@ -1,3 +1,28 @@
+import { Meteor } from 'meteor/meteor'; 
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Names = new Mongo.Collection('names');
+
+Meteor.methods({
+    'names.insert'(text) {
+        check(text, String);
+
+        // Make sure the user is logged in before inserting a task
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Names.insert({
+            text,
+            createdAt: new Date(),
+            owner: this.userId,
+            username: Meteor.users.findOne(this.userId).username,
+        });
+    },
+    'names.remove'(nameId) {
+        check(nameId, String);
+
+        Names.remove(nameId);
+    },
+});
