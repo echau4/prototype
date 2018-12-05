5d416977e91988a60d2ed35fd57372806bb3a05b
echau4 <31198647+echau4@users.noreply.github.com>
2018-12-05 11:12:39 -0600

    Fixed insert method to take 3 inputs and added private method to privatize certian info
    also added method so that it only searches database for that private info for that logged in user who owns that info

--- imports/api/names.js
+++ imports/api/names.js
@@ -4,9 +4,24 @@ import { check } from 'meteor/check';
 
 export const Names = new Mongo.Collection('names');
 
+if (Meteor.isServer) {
+    // This code only runs on the server
+    // Only publish names that are public or belong to the current user
+    Meteor.publish('names', function namesPublication() {
+        return Names.find({
+            $or: [
+                { private: { $ne: true } },
+                { owner: this.userId },
+            ],
+        });
+    });
+}
+
 Meteor.methods({
-    'names.insert'(text) {
-        check(text, String);
+    'names.insert'(data, data1, data2) {
+        // check(data, String);
+        // check(data1, String);
+        // check(data2, Number);
 
         // Make sure the user is logged in before inserting a task
         if (! this.userId) {
@@ -14,7 +29,9 @@ Meteor.methods({
         }
 
         Names.insert({
-            text,
+            firstName: data,
+            lastName: data1,
+            ssn: data2,
             createdAt: new Date(),
             owner: this.userId,
             username: Meteor.users.findOne(this.userId).username,
@@ -23,6 +40,24 @@ Meteor.methods({
     'names.remove'(nameId) {
         check(nameId, String);
 
+        const name = Names.findOne(nameId);
+        if (name.private && name.owner !== this.userId) {
+            // If the name is private, make sure only the owner can delete it
+            throw new Meteor.Error('not-authorized');
+        }
         Names.remove(nameId);
+    },
+    'names.setPrivate'(nameId, setToPrivate) {
+        check(nameId, String);
+        check(setToPrivate, Boolean);
+
+        const name = Names.findOne(nameId);
+
+        // Make sure only the name owner can make a name private
+        if (name.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Names.update(nameId, { $set: { private: setToPrivate } });
     },
 });
